292af077b2a98a88d8a09d6b5496951b9f938e1b
fanxux <fan.x@hotmail.de>
2022-09-09 19:47:24 +0200

    Add burger menu animation

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,2 +1,14 @@
-const msg: string = "Hello TypeScript";
-console.log(msg);
+window.onload = () => {
+    const $menu = document.querySelector('.menu');
+    const $menuBtn = document.querySelector('.menu__btn');
+
+    $menuBtn.addEventListener('click', function () {
+        if ($menu.classList.contains('is-open')) {
+            $menu.classList.remove('is-open');
+            $menu.classList.add('is-closed');
+        } else if ($menu.classList.contains('is-closed')) {
+            $menu.classList.add('is-open');
+            $menu.classList.remove('is-closed');
+        }
+    })
+};
